b4ca58bbf3409510c6bfb2b92564825fd38e0ecc
刘银萍 <liuyp@shk.com>
2017-11-16 17:29:29 +0800

    canvas 绘制 拖动

--- js/timeLine_canvas.js
+++ js/timeLine_canvas.js
@@ -13,7 +13,7 @@ var timeLine = {
      */
     initialize: function (elem, options) {
         console.log('initialize');
-        var self = this;
+        //var self = this;
         //参数配置
         if (!options) {
             options = {}
@@ -39,7 +39,7 @@ var timeLine = {
         //设置宽高
         this.options = {
             script_path: "",
-
+            //整体timeline 除了 右侧数据的部分
             controller_width_sp: 425,
             //控制器部分宽高
             controller_width: 300,
@@ -47,37 +47,33 @@ var timeLine = {
             //时间轴部分宽高
             timeline_width: 1000,
             timeline_height: 75,
+            //时间轴 距离 顶部的 高
+            timeline_height_top: 55,
             //默认背景颜色
             default_bg_color: "black",
-
             //默认当前时间
             default_time: moment.utc(),
             //默认一格的毫秒数目
             default_timespan: 24 * 60 * 60 * 1000,
-            mode_list: ['year', 'month', 'day', 'hour', 'minute'],
+            //年　月　日　小时
+            mode_list: ['year', 'month', 'day', 'hour'],
             //在当前模式下，选择精确到的 单位
-            mode_unit: ['month', 'day', 'hour', 'minute', 'second'],
+            mode_unit: ['month', 'day', 'hour', 'minute'],
             //每一个单位大小
-            mode_timespan: [12.5, 12.5, 12.5, 12.5, 5, 2.5],
+            mode_timespan: [12.5, 12.5, 12.5, 5],
 
-            select_modeindex: 1,
+            select_modeindex: 2,
             select_date: moment.utc(),
-
-
             //动画控制部分
             //是否拖拽
             is_drag: false,
             //开始拖拽x
             begin_X: 0,
             end_X: 0
-
-
         };
-
         //初始化全部显示
         this.init_container();
         //
-
         window.onresize = this._resizeScreen;
 
     },
@@ -181,18 +177,20 @@ var timeLine = {
 
     //位移过程
     moving: function () {
+        //如果正在位移 则对拖动进行处理
         if (timeLine.options.is_drag) {
             //是否正在拖动
-            //timeLine.options.is_drag = false;
             timeLine.options.end_X = event.offsetX;
             var drag_pix = timeLine.options.end_X - timeLine.options.begin_X;
-
-            console.log(drag_pix);
             //根据阈值进行赋值
             if (drag_pix > 20 || drag_pix < -20) {
                 timeLine._drag(drag_pix);
                 timeLine.options.begin_X = timeLine.options.end_X;
             }
+        }
+        else {
+            //若未进行位移，根据开始和参数计算当前时间 显示
+
         }
     },
 
@@ -216,7 +214,7 @@ var timeLine = {
 
     init_beginTime: function () {
         var selectMode = this.options.select_modeindex;
-        var modeName = this.options.mode_list [selectMode];
+        // var modeName = this.options.mode_list [selectMode];
         //在当前模式下，选择精确到的 单位
         var selectmodeunit = this.options.mode_unit[selectMode];
         //每一个单位大小
@@ -235,46 +233,44 @@ var timeLine = {
         timeLine.options.moment_end = moment_end.add(unitNum_after, selectmodeunit);
     },
 
+    //根据位移重新计算开始结束
     _getTimeSpace: function (trans) {
         var moment_begin = timeLine.options.moment_begin;
         var moment_end = timeLine.options.moment_end;
-        //计算需要用到的参数
-
         var selectMode = this.options.select_modeindex;
-        var modeName = this.options.mode_list [selectMode];
         //在当前模式下，选择精确到的 单位
-        var selectmodeunit = this.options.mode_unit  [selectMode];
+        var selectmodeunit = this.options.mode_unit[selectMode];
         //每一个单位大小
-        var selectmodetimespan = this.options.mode_timespan  [selectMode];
-        //    console.log(timeLine.options.moment_begin.format("YYYY-MM-DD HH:mm:ss"));
+        var selectmodetimespan = this.options.mode_timespan[selectMode];
         moment_begin.add(-trans / selectmodetimespan, selectmodeunit);
         moment_end.add(-trans / selectmodetimespan, selectmodeunit);
-        //  console.log(timeLine.options.moment_begin.format("YYYY-MM-DD HH:mm:ss"));
-
     },
     //根据当前时刻进行位移
     _drag: function (trans) {
         //根据当前trans 计算新的开始结束
 
-
         //基于当前时间计算
         timeLine._getTimeSpace(trans);
         var moment_begin = timeLine.options.moment_begin;
-
-
         var moment_end = timeLine.options.moment_end;
         timeLine._draw_bg();
+
+        var selectMode = this.options.select_modeindex;
+        //在当前模式下，选择精确到的 单位
+        var selectmodeunit = this.options.mode_unit  [selectMode];
+        var selectmode_pix = this.options.mode_timespan  [selectMode];
         //  this._draw_year(moment_begin, moment_end, this.options.select_date, "month", 12.5);
-        timeLine._draw_timeline(moment_begin, moment_end, this.options.select_date, "day", 12.5);
+        timeLine._draw_timeline(moment_begin, moment_end, this.options.select_date, selectmodeunit, selectmode_pix);
 
     },
 
+    //根据开始结束时间 绘制时间轴
     _draw_timeline: function (begin_date, end_date, select_date, unit_str, unit_pix) {
 
 
 
         //绘制横线
-        timeLine._canvas_line("white", 0, 55, timeLine.options.timeline_width, 55);
+        timeLine._canvas_line("white", 0, timeLine.options.timeline_height_top, timeLine.options.timeline_width, timeLine.options.timeline_height_top);
         //绘制年间隔
         //   timeLine._canvas_arc('white', 10, 55, 7);
 
@@ -287,35 +283,43 @@ var timeLine = {
         while (moment_draw < end_date) {
             //绘制圆形
             if (timeLine._getArcMode(moment_draw)) {
-                timeLine._canvas_arc("white", trans - unit_pix, 55, 7);
-                timeLine._canvas_line("white", trans - unit_pix, 3, trans - unit_pix, 75);
-                timeLine._canvas_txt("white", moment_draw.format("YYYY-MM-DD HH:mm:ss"), trans - unit_pix + 2, 75);
+                timeLine._canvas_arc("white", trans - unit_pix, timeLine.options.timeline_height_top, 7);
+                timeLine._canvas_line("white", trans - unit_pix, 3, trans - unit_pix, timeLine.options.timeline_height);
+                var txtStr = timeLine._getDateStr(moment_draw);
+                timeLine._canvas_txt("white", txtStr, trans - unit_pix + 5, timeLine.options.timeline_height - 3);
             }
 
             if (timeLine._getLongLineMode(moment_draw)) {
-                timeLine._canvas_line("white", trans, 35, trans, 55);
+                timeLine._canvas_line("white", trans, timeLine.options.timeline_height_top - 20, trans, timeLine.options.timeline_height_top);
             }
             else {
                 // (fillcolor, begin_point_x, begin_point_y, end_point_x, end_point_y)
-                timeLine._canvas_line("white", trans, 45, trans, 55);
+                //  timeLine._canvas_line("white", trans, timeLine.options.timeline_height_top - 10, trans, timeLine.options.timeline_height_top);
+                timeLine._canvas_dashed_line("white", trans, timeLine.options.timeline_height_top - 10, trans, timeLine.options.timeline_height_top);
+
             }
 
             moment_draw.add(1.0, unit_str);
+            // 绘制 时间块
+
             trans = trans + unit_pix;
         }
         //绘制圆点
-
-
     },
 
+    //根据选择时间 获取当前绘制时次是否为选择的时次
+    _getisSelectDate: function (select_moment, draw_moment) {
 
+    },
     _getArcMode: function (compareDate) {
         var selectMode = timeLine.options.mode_list[timeLine.options.select_modeindex];
         var isBegin = false;
+
         switch (selectMode) {
             case "year":
             {
-                if (compareDate.month() === 0) {
+                console.log(compareDate.month());
+                if (compareDate.month() === 1) {
                     isBegin = true;
                 }
                 break;
@@ -330,14 +334,14 @@ var timeLine = {
             }
             case "day":
             {
-                if (compareDate.hour() === 0) {
+                if (compareDate.hour() === 1) {
                     isBegin = true;
                 }
                 break;
             }
             case "hour":
             {
-                if (compareDate.minute() === 0) {
+                if (compareDate.minute() === 1) {
                     isBegin = true;
                 }
                 break;
@@ -353,6 +357,47 @@ var timeLine = {
                 break;
         }
         return isBegin;
+    },
+
+
+    //根据当前设置的 模式 返回对应长度的 Str
+    _getDateStr: function (moment) {
+        var selectMode = this.options.select_modeindex;
+        var modeName = this.options.mode_list [selectMode];
+        var moment_Str = moment.format("YYYY-MM-DD HH:mm:ss");
+        switch (modeName) {
+            case "year":
+            {
+                moment_Str = moment.format("YYYY");
+                break;
+            }
+            case "month":
+            {
+                moment_Str = moment.format("YYYY-MM");
+                break;
+            }
+            case "day":
+            {
+                moment_Str = moment.format("YYYY-MM-DD");
+                break;
+            }
+            case "hour":
+            {
+                moment_Str = moment.format("YYYY-MM-DD HH");
+                break;
+            }
+            case "minute":
+            {
+                moment_Str = moment.format("YYYY-MM-DD HH:mm");
+                break;
+            }
+            default:
+            {
+                break;
+            }
+
+        }
+        return moment_Str;
     },
 
     _getLongLineMode: function (compareDate) {
@@ -423,7 +468,22 @@ var timeLine = {
             var context = _canvas.getContext('2d');
             context.beginPath();
             context.strokeStyle = fillcolor;
-            context.lineWidth = 0.5;
+            context.lineWidth = 1;
+            context.moveTo(begin_point_x, begin_point_y);
+            context.lineTo(end_point_x, end_point_y);
+            context.closePath();
+            context.stroke();
+        }
+    },
+
+    _canvas_dashed_line: function (fillcolor, begin_point_x, begin_point_y, end_point_x, end_point_y) {
+        var _canvas = timeLine._el.timecanvas;
+        if (_canvas) {
+            var context = _canvas.getContext('2d');
+            context.beginPath();
+            context.strokeStyle = fillcolor;
+            context.lineWidth = 1;
+            //context.setLineDash([2]);
             context.moveTo(begin_point_x, begin_point_y);
             context.lineTo(end_point_x, end_point_y);
             context.closePath();
